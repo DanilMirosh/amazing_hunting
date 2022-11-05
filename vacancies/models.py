46d26c8f279193fe60b9fdcde985c6ec49618b70
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-05 21:37:40 +0500

    adminka

--- vacancies/models.py
+++ vacancies/models.py
@@ -13,3 +13,7 @@ class Vacancy(models.Model):
     text = models.CharField(max_length=2000)
     status = models.CharField(max_length=6, choices=STATUS, default="draft")
     create = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.slug
