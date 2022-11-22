44d1df43296b07e5c05063dba0c4fdf0966f9967
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-22 17:57:59 +0500

    fix viewsset

--- vacancies/views.py
+++ vacancies/views.py
@@ -4,15 +4,21 @@ from django.db.models import Count, Avg
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from amazing_hunting import settings
-from vacancies.models import Vacancy
+from vacancies.models import Vacancy, Skill
 from vacancies.serializers import VacancyListSerializer, VacancyDetailSerializer, VacancyCreateSerializer, \
-    VacancyUpdateSerializer, VacancyDestroySerializer
+    VacancyUpdateSerializer, VacancyDestroySerializer, SkillSerializer
 
 
 def hello(request):
     return HttpResponse("Hello world!")
+
+
+class SkillsViewSet(ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
 
 
 class VacancyListView(ListAPIView):
