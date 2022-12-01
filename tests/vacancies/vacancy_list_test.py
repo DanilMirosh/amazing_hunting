46a5ea40672c5deff3fd48f79d906740731025e3
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-12-01 17:08:52 +0500

    fix

--- tests/vacancies/vacancy_list_test.py
+++ tests/vacancies/vacancy_list_test.py
@@ -2,26 +2,21 @@ from datetime import date
 
 import pytest
 
-from vacancies.models import Vacancy
-
 
 @pytest.mark.django_db
-def test_vacancy_list(client):
-    vacancy = Vacancy.objects.create(
-        slug="123",
-        text="123",
-    )
+def test_vacancy_list(client, vacancy):
+
     expected_response = {
         "count": 1,
         "next": None,
         "previous": None,
         "results": [{
             "id": vacancy.pk,
-            "text": "123",
-            "slug": "123",
+            "text": "test",
+            "slug": "test text",
             "status": "draft",
             "created": date.today().strftime("%Y-%m-%d"),
-            "username": None,
+            "username": "test",
             "skills": []
         }]
     }
