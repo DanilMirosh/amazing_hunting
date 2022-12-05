9d9a3e4176ffd9d63cc72a39c55078631f25f38f
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-12-05 11:25:02 +0500

    add swagger

--- amazing_hunting/settings.py
+++ amazing_hunting/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'vacancies',
     'companies',
     'authentication',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -167,7 +168,14 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Hunting API",
+    "DESCRIPTION": "Awesome hunting API",
+    "VERSION": "1.0.0",
 }
 
 AUTH_USER_MODEL = 'authentication.User'
